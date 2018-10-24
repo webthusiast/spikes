acb4930a03a101fecb25a3e30e40ba49789083c2
Roald de Vries <roald@webthusiast.nl>
2018-10-24 23:44:17 +0200

    create user account besides root

--- digitalocean/init.sh
+++ digitalocean/init.sh
@@ -1,2 +1,12 @@
-#!/bin/sh
-echo "Hello World. The time is now $(date -R)!" | tee /root/output.txt
+#!/bin/bash
+
+set -x
+
+user=roald
+
+adduser --disabled-login --gecos '' $user
+usermod -aG sudo $user
+sudo -u $user -s -- <<EOF
+  mkdir ~$user/.ssh
+  printf '$(cat ~/.ssh/authorized_keys)' >/home/$user/.ssh/authorized_keys
+EOF
