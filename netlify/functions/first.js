8cc0414cee49e7b20642f4fe4fb8df595c360066
webthusiast <github@roalddevries.nl>
2023-05-19 16:11:14 +0000

    Access a doc in Fauna (first attempt)

--- netlify/functions/first.js
+++ netlify/functions/first.js
@@ -1,6 +1,12 @@
+const faunadb = require('faunadb');
+const {Get, Ref, Collection} = faunadb.query;
+
 exports.handler = async function (event, context) {
+    const client = new faunadb.Client({ secret: process.env.FAUNA_KEY });
+    const result = await client.query(Ref(Collection("customers"), "101"));
+    console.log(result);
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello World", time: new Date().toISOString() }),
+        body: JSON.stringify({ message: "Hello World", time: new Date().toISOString(), data: result.data }),
     };
 };
